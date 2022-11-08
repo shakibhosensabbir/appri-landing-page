14812ddcf25e699d1471b83573cbde906aeda65e
shakibhosensabbir <shakibhossin1234@gmail.com>
2022-11-08 23:37:06 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function ($){
+    "use strict"
+
+     
+
+
+
+
+
+
+})(JQuery);
+
+
+
+
+// video pop up
+
+ $(".youtube-link").grtyoutube(); 
+ $(".youtube-link").grtyoutube({ autoPlay:false }); 
+$(".youtube-link").grtyoutube({ theme:"light" }); 
+
+
